86d98908b8d86eb5dafbaf9bd9ec5fc991fb4ca7
gitAdd <1305177077@qq.com>
2018-06-11 11:35:33 +0800

    提交interview-code 在element用法上

--- src/components/element_comp.vue
+++ src/components/element_comp.vue
@@ -9,6 +9,42 @@
         <el-progress status='success' :percentage='100'></el-progress>
         <el-progress color='#cccccc' :percentage='60'></el-progress>
         <el-progress color='#dddddd' :percentage='60' type='circle'></el-progress>
+
+        <h4>测试display:none跟visibility:hidden还有opacity:0的区别.</h4>
+        <h6>1:控件占据问题</h6>
+        <ul>
+          <li>display:none ->彻底消失,不在文档流中占位,浏览器也不会解析该元素,visibility:hidden是视觉上消失了,在文档流中占位,浏览器会解析该元素</li>
+          <li>visibility:hidden在性能上高于display:none,是因为display切换时,页面产生回流[当页面中的一部分元素需要改变尺寸,布局,显示隐藏等,页面重新构建等,属于回流]</li>
+          <li>display会引起回流跟重绘,而visibility跟opacity只进行重绘.</li>
+        </ul>
+        <div class="div1" @click='btnEvent()'>  
+          <p>display:none</p>  
+        </div>  
+        <div class="div2" @click='btnEvent()'>  
+            <p>visibility:visible</p>  
+        </div>  
+        <div class="div3" @click='btnEvent()'>  
+            <p>opacity:0</p>  
+        </div>  
+        <div>测试display,visibility,opacity是否占据文档流</div>
+        <h6>2: 子继承问题</h6>
+        <ul>
+          <li>display:none:不会被子元素继承,但是父亲不在了,子自然就不会显示了.皮之不存,毛之安附</li>
+          <li>visibility: hidden,会被子继承,可通过visibility:visible使子元素显示出来.</li>
+          <li>opacity:0,也会被子元素继承,但是不能通过opacity:0使其子元素重新显示.</li>
+        </ul>
+        <h6>3.事件绑定的问题,本文的例子还涉及到事件冒泡</h6>
+        <ul>
+          <li>display:none 的元素都已经不再页面存在了,因此肯定也无法触发它上面绑定的事件</li>
+          <li>visibility:hidden 元素上绑定的事件也无法触发</li>
+          <li>opacity: 0元素上面绑定的事件是可以触发的</li>
+        </ul>
+
+
+       <h4>jquery的几种事件绑定方法</h4>
+       <input type="button" value="按钮" class="btn-test" />
+       <div class="container">
+       </div>
     </div>
 </template>
 <script>
@@ -29,6 +65,9 @@ export default {
     },
     addTag() {
       //实现标签的追加
+    },
+    btnEvent() {
+      alert("事件绑定");
     }
   },
   created: function() {},
@@ -83,6 +122,32 @@ export default {
   width: 400px;
   height: 20px;
   padding-top: 5px;
+}
+.element_comp .div1,
+.element_comp .div2,
+.element_comp .div3 {
+  width: 200px;
+  height: 200px;
+  background: #ccc;
+  margin-top: 10px;
+}
+.element_comp .div1 {
+  display: none;
+}
+.element_comp .div1 p {
+  display: block;
+}
+.element_comp .div2 {
+  visibility: hidden;
+}
+.element_comp .div2 p {
+  visibility: visible;
+}
+.element_comp .div3 {
+  opacity: 0;
+}
+.element_comp .div3 p {
+  opacity: 1;
 }
 </style>
 
